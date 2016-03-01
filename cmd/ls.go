9b028ddd03ec0425aaf893cf694aa25189694ec2
frankw <fwessels@xs4all.nl>
2016-03-01 16:58:13 +0100

    Skeleton for list

--- cmd/ls.go
+++ cmd/ls.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/fwessels/s3git-go"
 	"github.com/spf13/cobra"
 )
 
@@ -12,8 +13,15 @@ var lsCmd = &cobra.Command{
 	Short: "List files in the repository",
 	Long: "List files in the repository",
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("ls called")
+
+		repo, err := s3git.OpenRepository(".")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		for elem := range repo.List() {
+			fmt.Println(elem)
+		}
 	},
 }
 
