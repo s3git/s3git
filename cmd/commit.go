81154604e54642861c39a585d4e006ff432d82ab
frankw <fwessels@xs4all.nl>
2016-03-04 13:42:10 +0100

    Basic implementation of commit command

--- cmd/commit.go
+++ cmd/commit.go
@@ -2,7 +2,8 @@ package cmd
 
 import (
 
-	_ "github.com/s3git/s3git-go"
+	"fmt"
+	"github.com/s3git/s3git-go"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +16,21 @@ var commitCmd = &cobra.Command{
 	Long: "Commit the changes in the repository",
 	Run: func(cmd *cobra.Command, args []string) {
 
-		// TODO: Add commit command
+		repo, err := s3git.OpenRepository(".")
+		if err != nil {
+			er(err)
+		}
+
+		key, nothing, err := repo.Commit(message)
+		if err != nil {
+			er(err)
+		}
+		if nothing {
+			fmt.Println("Nothing to commit")
+		} else {
+			fmt.Printf("[master%s]\n", key)
+			fmt.Printf("X files added, Y files removed\n")
+		}
 	},
 }
 
